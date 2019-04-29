4443558bbdc59a8503afdfa0d48111634649273c
Crappyjacks <49409342+Crappyjacks@users.noreply.github.com>
2019-04-29 12:31:38 -0500

    Add files via upload

--- uno.js
+++ uno.js
@@ -3,7 +3,7 @@
  *
  *  Author(s): Joseph Canning (jec2)
  *  Description:
- *      This file is part of an implementation of Mattel's card game Uno and runs a server on a hard-coded port that accepts four players.
+ *      This file is part of an implementation of Mattel's card game Uno and runs a server on a hard-coded port that accepts up to ten players.
 */
 
 // non-function variables
@@ -14,15 +14,27 @@ const port = 5555;
 const colors = ['red', 'blue', 'yellow', 'green'];
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // zero is excluded for convenience; see initCards
 const actions = ['draw2', 'draw4', 'skip', 'reverse', 'wild'];
+const playerCap = 10;
 let players = []; // array of clients on server
 let cards = []; // array of all cards; effectively final after initialization
 let deck = []; // shuffled deck of cards that are dealt to the players
 let turn = 0; // player whose turn it is
 let reverse = false; // turn order is reversed
+let gameStarted = false; // game is in progress
 var match; // card whose number, color, or action must be matched
 
 // functions
-const getPlayerNum = client => players.indexOf(client) + 1; // convert index of client in players to player number visible to clients and return it
+function getPlayerNum(client) { // convert index of client in players to player number visible to clients and return it
+
+    let playerNum = players.findIndex(player => player.socket.id === client.id);
+
+    if (playerNum === -1) {
+        return -1;
+    } else {
+        return playerNum + 1;
+    }
+
+}
 
 function Card(num, color, action) { // Card constructor
 
@@ -30,6 +42,14 @@ function Card(num, color, action) { // Card constructor
     this.color = color; // face color
     this.action = action; // draw two, skip, etc.
     this.imgID = '' + num + color + action; // name of image representing this card
+
+}
+
+function Player(socket, cards, num) { // Player constructor
+
+    this.socket = socket; // player's web socket
+    this.cards = cards; // number of cards
+    this.num = num; // player number
 
 }
 
@@ -45,11 +65,6 @@ function initCards() { // creates the 108 Uno cards
         });
 
         actions.forEach(action => cards.push(new Card(null, color, action)));
-
-    });
-
-    colors.forEach(color => {
-
         cards.push(new Card(0, color, null));
         cards.push(new Card(null, color, 'draw2'));
         cards.push(new Card(null, color, 'skip'));
@@ -107,27 +122,49 @@ console.log('Server is online; waiting for players');
 // server responses to client transmissions
 io.on('connection', client => {
 
-    client.on('join', msg => { // client has joined the server; give them seven cards
+    if (players.length + 1 > playerCap) { // server is full; reject further connections
 
-        players.push(client);
-        io.to(players[turn].id).emit('take cards', JSON.stringify(dealCards(7)));
+        client.emit('reject', 'Server is full');
+        client.disconnect();
+        console.log(`Client ${client.id} rejected: server is full`);
+        return;
+
+    }
+
+    if (gameStarted) { // first player started the game; reject further connections
+
+        client.emit('reject', 'Game in progress');
+        client.disconnect();
+        console.log(`Client ${client.id} rejected: game in progress`);
+        return;
+
+    }
+
+    client.on('join', msg => { // client has joined the server; give them seven cards and let others know
+
+        players.push(new Player(client, 7, players.length + 1));
+        client.emit('assign', players.length);
+        client.emit('take cards', JSON.stringify(dealCards(7)));
+        client.broadcast.emit('op update', JSON.stringify(players));
         console.log(msg);
         console.log(`\n${players.length} clients now connected:`);
-        let i = 0;
-        players.forEach(client => console.log('\t' + i++ + ': ' + client.id));
+
+        if (players.length === 1) {
+            client.emit('host');
+        }
 
     });
-
     
     client.on('clicked', msg => console.log(msg + ` (client ${client.id})`)); // TODO: remove once actual GUI is being built; this is just an example event
 
     client.on('disconnect', msg => {  // client has left the server
         
-        players.splice(players.indexOf(client), 1);
+        players.splice(players.indexOf(players.findIndex(player => player.socket.id === client.id)), 1);
+        client.broadcast.emit('op update', JSON.stringify(players));
         console.log(msg);
         console.log(`\n${players.length} clients still connected:`);
         let i = 0;
-        players.forEach(client => console.log('\t' + ++i + ': ' + client.id));
+        players.forEach(player => console.log('\t' + ++i + ': ' + player.socket.id));
     
     });
 
@@ -135,7 +172,7 @@ io.on('connection', client => {
     client.on('played', card => { // when client plays a card, relay sent card JSON string to all other clients; tell next player to go
 
         match = card;
-        io.broadcast.emit('match', JSON.stringify(match));
+        client.broadcast.emit('match', JSON.stringify(match));
 
         if (reverse) {
 
@@ -155,18 +192,26 @@ io.on('connection', client => {
 
         }
 
-        io.to(players[turn].id).emit('play');
+        io.to(`${players[turn].socket.id}`).emit('play');
 
     });
 
     client.on('draw', num => { // when client needs to draw cards, send them a card array JSON string; inform all other clients who took how many cards
 
-        io.to(players[turn].id).emit('take cards', JSON.stringify(dealCards(num)));
-        io.broadcast.emit('op draw', getPlayerNum(client), num);
+        client.emit('take cards', JSON.stringify(dealCards(num)));
+        client.broadcast.emit('op draw', getPlayerNum(client), num);
 
     });
 
     client.on('win', () => io.broadcast.emit('lose', getPlayerNum(client))); // player has won the game; tell others who they lost to
+
+    client.on('start', () => { // when first player starts game, tell all other clients
+
+        client.broadcast.emit('ready', 'The game has been started');
+        gameStarted = true;
+
+
+    });
 
 });
 
